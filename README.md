62dadb035f18a777ba6eae38030ad907824a417e
Matt Bower <matt@webbower.com>
2014-01-24 17:59:37 -0800

    - Fix README - Add code comments and reformat

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 This plugin is a utility method for jQuery to convert a string value passed to it to one of the following JavaScript scalar primitives:
 
-* "false" -> false (Bool)
-* "true" -> true (Bool)
-* "null" -> null
-* "" -> undefined
-* "<number>" -> <number> (Number)
+* `"false"` -> `false` (Bool)
+* `"true"` -> `true` (Bool)
+* `"null"` -> `null`
+* `""` -> `undefined`
+* `"<number>"` -> `<number>` (Number)
 
 It will not cast Array or Object literals. This would mainly be used for properly casting values you would pull from form fields or other HTML attributes like data-* attributes.
 
--- jquery.cast.js
+++ jquery.cast.js
@@ -1,22 +1,37 @@
 ; (function($, undefined) {
 	$.cast = function(str) {
+		// If a non-string value was passed in, return that value, unmodified
 		if(typeof str !== 'string')
+		{
 			return str;
-		
+		}
+
 		var ret;
 		
-		if(str === 'true')
+		// true boolean
+		if(str === 'true') {
 			ret = true;
-		else if(str === 'false')
+		}
+		// false boolean
+		else if(str === 'false') {
 			ret = false;
-		else if(str === 'null')
+		}
+		// null
+		else if(str === 'null') {
 			ret = null;
-		else if(str.length === 0)
+		}
+		// empty string for undefined (might change this)
+		else if(str.length === 0) {
 			ret = undefined;
-		else if(!isNaN(+str))
+		}
+		// Number
+		else if(!isNaN(+str)) {
 			ret = +str;
-		else
+		}
+		// Matches none of the above so just return the original string
+		else {
 			ret = str;
+		}
 		
 		return ret;
 	};
